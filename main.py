187110ad3e94f44d17d5aea77dbfd85eb96ff807
Josephine1000j <148771304+Josephine1000j@users.noreply.github.com>
2024-09-25 10:09:54 +0200

    lister

--- main.py
+++ main.py
@@ -1,16 +1,89 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+boardsDict = {
+    #1-4 er komplette boards
+    "1.jgp":36,
+    "2.jgp":43,
+    "3.jgp":52,
+    "4.jgp":42,
+    #5-8 er samme som 1-4 men med slotte
+    "5.jpg":36,
+    "6.jpg":43,
+    "7.jpg":52,
+    "8.jpg":42,
+    #9-12 er inkomplette boards
+    "9.jpg":45,
+    "10.jpg":34,
+    "11.jpg":49,
+    "12.jpg":22,
+    #13-16 er samme som 9-12 men med slot
+    "13.jpg":45,
+    "14.jpg":34,
+    "15.jpg":49,
+    "16.jpg":22,
+    #17-20 er uskarpe billeder
+    "17.jpg":20,
+    "18.jpg":60,
+    "19.jpg":36,
+    "20.jpg":52,
+    #21-24 er samme som 17-20 men med slot
+    "21.jpg":20,
+    "22.jpg":60,
+    "23.jpg":36,
+    "24.jpg":52,
+    #25-28 brikker ligger ikke helt lined up
+    "25.jpg":44,
+    "26.jpg":48,
+    "27.jpg":67,
+    "28.jpg":59,
+    #29-32 er samme som 25-28 men med slot
+    "29.jpg":44,
+    "30.jpg":48,
+    "31.jpg":67,
+    "32.jpg":59,
+    #33-36 er mørkere, nogle fulde plader og nogle liner dårlig op
+    "33.jpg":21,
+    "34.jpg":36,
+    "35.jpg":46,
+    "36.jpg":51,
+    #37-40 er samme som 33-36 men med slot
+    "37.jpg":21,
+    "38.jpg":36,
+    "39.jpg":46,
+    "40.jpg":51,
+    #41-43 kan jeg ikke lige se hvad der er med
+    "41.jpg":33,
+    "42.jpg":43,
+    "43.jpg":66,
+    #44 er samme som 41 men med slot
+    "44.jpg":33,
+    #nu tror jeg det begynder at være lidt en blanding af alting
+    "45.jpg":38,
+    "46.jpg":43,
+    "47.jpg": 66,
+    "48.jpg": 45,
+    "49.jpg": 26,#den rigtige antal kroner for 53
+    "50.jpg": 34,
+    "51.jpg": 37,
+    "52.jpg": 45,
+    "53.jpg": 23, #Tre ekstra kroner gemmer sig bag ved slottet
+    "54.jpg": 34,
+    "55.jpg": 37,
+    "56.jpg": 44,
+    "57.jpg": 64, #blurred
+    "58.jpg": 36,
+    "59.jpg": 37,
+    "60.jpg": 44,
+    "61.jpg": 64, #blurred
+    "62.jpg": 36, #blurred
+    "63.jpg": 37,
+    "64.jpg": 66,
+    "65.jpg": 80,
+    "66.jpg": 124,
+    "67.jpg": 99,
+    "68.jpg": 66,
+    "69.jpg": 124,
+    "70.jpg": 99,
+    "71.jpg": 66,
+    "72.jpg": 80,
+    "73.jpg": 124,
+    "74.jpg": 99
+}
